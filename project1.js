d0228196feff2c6a03ce1181175837b8aaa116bd
bepoy <nuzhatrahman333@gmail.com>
2025-10-21 21:04:11 +0600

    Meow

---
+++ project1.js
@@ -0,0 +1,46 @@
+let btn = document.querySelector('#new-quote');
+let quote = document.querySelector('.quote');
+let person = document.querySelector('.person');
+
+const quotes = [ {
+    quote: `"fat."`,
+    person: `-Vivi`
+},
+
+{
+    quote: `"Beep-poy"`,
+    person: `-Rk`
+},
+
+{
+    quote: `"Rotund"`,
+    person: `-Retardo`
+},
+
+{
+    quote: `"beppy is smarter than most cats"`,
+    person: `-Hahamid`
+},
+
+{
+    quote: `"Fuckass ai users. They're beyond banning just kill them"`,
+    person: `-Guppi`
+},
+
+{
+    quote: `"beppy cute biral"`,
+    person: `-Mogammel`
+},
+
+{
+    quote: `"VERY gashu"`,
+    person: `-Rimco`
+},];
+
+btn.addEventListener('click', function() {
+
+let random = Math.floor(Math.random() * quotes.length); 
+
+quote.innerText = quotes[random].quote;
+person.innerText = quotes[random].person;
+})
